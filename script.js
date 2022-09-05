eba6d37d40cb3888c8c4fffcc1772fdfdf96b732
taehyun Kim <07taehyun.k@gmail.com>
2022-09-05 23:15:12 +0900

    웹 스크롤 효과 적용

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function deleteTxt(){
     if(currentTxt.length !== 0){
         setTimeout(deleteTxt, Math.floor(Math.random() * 100));
     }else{
-        index = (index + 1) % txtArr.length;
+        index = (index + 1) % txtArr.length; 
         currentTxt  = txtArr[index].split(" ");
         writeTxt();
     }
