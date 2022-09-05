98061449a6a580af90a00e9f9ec41539efaa26e5
taehyun Kim <07taehyun.k@gmail.com>
2022-09-05 23:06:23 +0900

    스크롤 이벤트 적용

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ function deleteTxt(){
 }
 writeTxt();
 })();
+
+const headerEl = document.querySelector("header");
+window.addEventListener('scroll', function(){
+    requestAnimationFrame(scrollCheck);
+});
+function scrollCheck(){
+    let browerScrollY = window.scrollY ? window.scrollY : window.pageYOffset;
+    if(browerScrollY > 0){
+        headerEl.classList.add('active');
+    }else{
+        headerEl.classList.remove("active");
+    }
+}
+
